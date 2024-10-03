a47dc6ea419742fb77052fe60b5e5156b3718d00
[ingdev2] <[ingdesarrollo2@bonnadona.co]>
2024-10-03 17:14:36 -0500

    repo ready

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -2,6 +2,11 @@ import React from "react";
 
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { ConfigProvider } from "antd";
+import themeConfig from "@/theme/themeConfig";
+import es_ES from "antd/locale/es_ES";
+import { Providers } from "@/redux/providers";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,7 +27,15 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
       </head>
 
       <body className={inter.className} style={{ margin: 0, padding: 0 }}>
-        <main className="container-main-app">{children}</main>
+        <Providers>
+          <main className="container-main-app">
+            <AntdRegistry>
+              <ConfigProvider theme={themeConfig} locale={es_ES}>
+                {children}
+              </ConfigProvider>
+            </AntdRegistry>
+          </main>
+        </Providers>
       </body>
     </html>
   );
